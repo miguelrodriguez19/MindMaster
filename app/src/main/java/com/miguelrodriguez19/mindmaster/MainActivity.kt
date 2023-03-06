d5383abe16574f590f987b4f5016f2eb0e006b0c
Miguel Rodríguez Herrero <rodriguezherreromiguel@gmail.com>
2023-03-06 13:00:01 +0100

    Root commit Anadidas todas las dependencias necesarias en una primera instancia.

---
+++ app/src/main/java/com/miguelrodriguez19/mindmaster/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.miguelrodriguez19.mindmaster
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
