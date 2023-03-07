df5f92ab031c9185e75b423efa9cbf9627ee6b58
Miguel Rodríguez Herrero <rodriguezherreromiguel@gmail.com>
2023-03-07 18:12:04 +0100

    Icono y splash Screen

--- app/src/main/java/com/miguelrodriguez19/mindmaster/MainActivity.kt
+++ app/src/main/java/com/miguelrodriguez19/mindmaster/MainActivity.kt
@@ -2,10 +2,15 @@ package com.miguelrodriguez19.mindmaster
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        val screenSplash = installSplashScreen()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        screenSplash.setKeepOnScreenCondition{
+            false
+        }
     }
 }
