5c93e9729917d6c2a36b9244361e2dc1b2989dec
Miguel Rodríguez Herrero <rodriguezherreromiguel@gmail.com>
2023-03-08 23:22:49 +0100

    avance front end + fragment_log_in.xml, añadidos casi todos los fragmentos, edicion de colores, mejora de splash,

--- app/src/main/java/com/miguelrodriguez19/mindmaster/MainActivity.kt
+++ app/src/main/java/com/miguelrodriguez19/mindmaster/MainActivity.kt
@@ -2,15 +2,26 @@ package com.miguelrodriguez19.mindmaster
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.annotation.IdRes
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.fragment.app.FragmentActivity
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.miguelrodriguez19.mindmaster.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         val screenSplash = installSplashScreen()
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         screenSplash.setKeepOnScreenCondition{
             false
         }
     }
+
 }
---
+++ app/src/main/java/com/miguelrodriguez19/mindmaster/passwords/PasswordsFragment.kt
@@ -0,0 +1,32 @@
+package com.miguelrodriguez19.mindmaster.passwords
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.miguelrodriguez19.mindmaster.R
+import com.miguelrodriguez19.mindmaster.databinding.FragmentPasswordsBinding
+
+class PasswordsFragment : Fragment() {
+    private var _binding: FragmentPasswordsBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentPasswordsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
