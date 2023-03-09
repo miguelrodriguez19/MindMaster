011c6667f91920dd1ba9f27728eb91d0ad6952f9
Miguel Rodríguez Herrero <rodriguezherreromiguel@gmail.com>
2023-03-09 01:31:51 +0100

    avance front end + fragment_sign_up.xml y union mutua con fragment_log_in.xml, retocado colores

--- app/src/main/java/com/miguelrodriguez19/mindmaster/welcome/LogInFragment.kt
+++ app/src/main/java/com/miguelrodriguez19/mindmaster/welcome/LogInFragment.kt
@@ -5,12 +5,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.findNavController
+import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton
 import com.miguelrodriguez19.mindmaster.R
 import com.miguelrodriguez19.mindmaster.databinding.FragmentLogInBinding
 
 class LogInFragment : Fragment() {
     private var _binding: FragmentLogInBinding? = null
     private val binding get() = _binding!!
+    private lateinit var etEmail:EditText
+    private lateinit var etPassword:EditText
+    private lateinit var btnLogIn: ExtendedFloatingActionButton
+    private lateinit var btnGoogle: ExtendedFloatingActionButton
+    private lateinit var btnFacebook: ExtendedFloatingActionButton
+    private lateinit var btnForgottenPwd: Button
+    private lateinit var btnSignUp: Button
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -23,6 +35,25 @@ class LogInFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        (requireActivity() as AppCompatActivity).supportActionBar?.hide()
+        initBindingElements()
+
+        btnSignUp.setOnClickListener {
+            val action = LogInFragmentDirections.actionLogInFragmentToSignUpFragment()
+            findNavController().navigate(action)
+        }
+
+
+    }
+
+    private fun initBindingElements() {
+        etEmail = binding.txtEmailLogIn
+        etPassword = binding.txtPasswordLogIn
+        btnLogIn = binding.efabLogin
+        btnGoogle = binding.efabGoogle
+        btnFacebook = binding.efabFacebook
+        btnForgottenPwd = binding.btnForgottenPassword
+        btnSignUp = binding.btnSignUp
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/miguelrodriguez19/mindmaster/welcome/SignUpFragment.kt
+++ app/src/main/java/com/miguelrodriguez19/mindmaster/welcome/SignUpFragment.kt
@@ -5,12 +5,32 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.EditText
+import android.widget.ImageButton
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.findNavController
+import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton
 import com.miguelrodriguez19.mindmaster.R
 import com.miguelrodriguez19.mindmaster.databinding.FragmentSignUpBinding
 
 class SignUpFragment : Fragment() {
     private var _binding: FragmentSignUpBinding? = null
     private val binding get() = _binding!!
+    private lateinit var etName: EditText
+    private lateinit var etSurname: EditText
+    private lateinit var etEmail: EditText
+    private lateinit var etPhone: EditText
+    private lateinit var etBirthdate: EditText
+    private lateinit var etPassword: EditText
+    private lateinit var etRepeatPassword: EditText
+    private lateinit var btnSignUp: ExtendedFloatingActionButton
+    private lateinit var btnGoogle: ExtendedFloatingActionButton
+    private lateinit var btnFacebook: ExtendedFloatingActionButton
+    private lateinit var btnLogIn: Button
+    private lateinit var btnBack: ImageButton
+    private lateinit var checkTerms: CheckBox
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -23,6 +43,32 @@ class SignUpFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        (requireActivity() as AppCompatActivity).supportActionBar?.hide()
+        initBindingElements()
+
+        btnLogIn.setOnClickListener {
+            findNavController().popBackStack()
+        }
+
+        btnBack.setOnClickListener {
+            findNavController().popBackStack()
+        }
+    }
+
+    private fun initBindingElements() {
+        etName = binding.txtName
+        etSurname = binding.txtSurname
+        etEmail = binding.txtEmail
+        etPhone = binding.txtPhone
+        etBirthdate = binding.txtDateBirthdate
+        etPassword = binding.txtPassword
+        etRepeatPassword = binding.txtRepeatPassword
+        btnSignUp = binding.efabSignup
+        btnGoogle = binding.efabGoogle
+        btnFacebook = binding.efabFacebook
+        checkTerms = binding.checkbox
+        btnLogIn = binding.btnLogin
+        btnBack = binding.btnBack
     }
 
     override fun onDestroyView() {
