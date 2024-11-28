bb7992370607e7aadb38b64f83dc92223a35f553
Miguel Rodríguez Herrero <rodriguezherreromiguel@gmail.com>
2024-11-28 10:01:40 +0100

    fix() null safety

--- app/src/main/java/com/miguelrodriguez19/mindmaster/model/comparators/EventGroupComparator.kt
+++ app/src/main/java/com/miguelrodriguez19/mindmaster/model/comparators/EventGroupComparator.kt
@@ -1,6 +1,7 @@
 package com.miguelrodriguez19.mindmaster.model.comparators
 
 import com.miguelrodriguez19.mindmaster.model.structures.dto.schedule.EventsResponse
+import com.miguelrodriguez19.mindmaster.model.structures.exceptions.ExceptionHolder
 import com.miguelrodriguez19.mindmaster.model.utils.DateTimeUtils.DEFAULT_DATE_FORMAT
 import java.text.SimpleDateFormat
 import java.util.Locale
@@ -10,6 +11,7 @@ class EventGroupComparator : Comparator<EventsResponse> {
         val dateFormat = SimpleDateFormat(DEFAULT_DATE_FORMAT, Locale.getDefault())
         val parsedDate1 = dateFormat.parse(e1.date)
         val parsedDate2 = dateFormat.parse(e2.date)
-        return parsedDate1.compareTo(parsedDate2)
+        return parsedDate1?.compareTo(parsedDate2)
+            ?: ExceptionHolder.illegalArgument("Date cannot be null")
     }
 }
--- app/src/main/java/com/miguelrodriguez19/mindmaster/model/comparators/MovementComparator.kt
+++ app/src/main/java/com/miguelrodriguez19/mindmaster/model/comparators/MovementComparator.kt
@@ -1,6 +1,7 @@
 package com.miguelrodriguez19.mindmaster.model.comparators
 
 import com.miguelrodriguez19.mindmaster.model.structures.dto.expenses.Movement
+import com.miguelrodriguez19.mindmaster.model.structures.exceptions.ExceptionHolder
 import com.miguelrodriguez19.mindmaster.model.utils.DateTimeUtils.DEFAULT_DATE_FORMAT
 import java.text.SimpleDateFormat
 import java.util.Locale
@@ -10,10 +11,11 @@ class MovementComparator : Comparator<Movement> {
         val dateFormat = SimpleDateFormat(DEFAULT_DATE_FORMAT, Locale.getDefault())
         val parsedDate1 = dateFormat.parse(obj1.date)
         val parsedDate2 = dateFormat.parse(obj2.date)
-        val result = parsedDate1.compareTo(parsedDate2)
-        return if (result != 0){
+        val result = parsedDate1?.compareTo(parsedDate2)
+            ?: ExceptionHolder.illegalArgument("Date cannot be null")
+        return if (result != 0) {
             result
-        }else{
+        } else {
             obj1.concept.compareTo(obj2.concept)
         }
     }
